c89f1e5f6711f3c0e92a824ad2c0dc2f354ba15b
Timothy Johnson <timothy.johnson@broadcom.com>
2019-06-03 15:26:59 -0400

    Move help viewer code up one level
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- help_viewer/src/tree.ts
+++ help_viewer/src/tree.ts
@@ -20,7 +20,7 @@ function loadTree(nodes: ITreeNode[]) {
     $("#jstree").jstree({
         core: {
             animation: 0,
-            check_callback: true,
+            check_callback: false,
             themes: {
                 icons: false,
                 multiple: false
@@ -29,6 +29,7 @@ function loadTree(nodes: ITreeNode[]) {
         },
         plugins: ["search", "wholerow"],
         search: {
+            fuzzy: true,
             show_only_matches: true,
             show_only_matches_children: true
         },
@@ -37,4 +38,5 @@ function loadTree(nodes: ITreeNode[]) {
     });
 
     $("#tree-search-input").on("change keyup mouseup paste", updateSearch);
+    
 }
