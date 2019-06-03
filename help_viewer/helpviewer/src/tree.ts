6eec6d4c483cf757471cc9cb6db4b579adb08af3
Timothy Johnson <timothy.johnson@broadcom.com>
2019-06-03 15:14:26 -0400

    Add JSTree to navigate docs
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

---
+++ help_viewer/helpviewer/src/tree.ts
@@ -0,0 +1,40 @@
+interface ITreeNode {
+    id: string;
+    text: string;
+    children: undefined | ITreeNode[];
+}
+
+let searchTimeout: number = 0;
+
+function updateSearch() {
+    if (searchTimeout) {
+        clearTimeout(searchTimeout);
+    }
+    searchTimeout = setTimeout(() => {
+        const searchText = $("#tree-search-input").val().toString();
+        $("#jstree").jstree(true).search(searchText);
+    }, 250);
+}
+
+function loadTree(nodes: ITreeNode[]) {
+    $("#jstree").jstree({
+        core: {
+            animation: 0,
+            check_callback: true,
+            themes: {
+                icons: false,
+                multiple: false
+            },
+            data: nodes
+        },
+        plugins: ["search", "wholerow"],
+        search: {
+            show_only_matches: true,
+            show_only_matches_children: true
+        },
+    }).on("changed.jstree", (e, data) => {
+        $("#docs-page").attr("src", `cmd_docs/${data.selected[0]}`);
+    });
+
+    $("#tree-search-input").on("change keyup mouseup paste", updateSearch);
+}
